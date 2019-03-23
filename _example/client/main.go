c3405be6c96d649987707c94a679ebc4510c77bd
root <amir.so@icloud.com>
2019-03-23 14:16:02 +0430

    cleanup

---
+++ _example/client/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:9090",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+}
